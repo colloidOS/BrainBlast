049077af60ed553659b0f4ed232e424a098d0c29
colloidOS <nnagoziemcollin@gmail.com>
2025-03-15 12:46:23 +0100

    Initial Commit

---
+++ BrainBlast Task/BrainBlast_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBlast_TaskApp.swift
+//  BrainBlast Task
+//
+//  Created by Macintosh HD on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BrainBlast_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainBlast TaskUITests/BrainBlast_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BrainBlast_TaskUITestsLaunchTests.swift
+//  BrainBlast TaskUITests
+//
+//  Created by Macintosh HD on 15/03/2025.
+//
+
+import XCTest
+
+final class BrainBlast_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
